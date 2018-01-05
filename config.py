6f15f8b376602a5dded362658bd28ae4d66566c6
Telawir <mikeleplacido@interia.pl>
2018-01-05 01:02:58 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,5 @@
 prefix = 'p.'
 ownerid = '215761992474951681'
-token = 'Mzg5ODM3NzY5MjkzMTY4NjQx.DSrQCQ.WxMQ8VtpN2aCSA4hW1hO0NG6HSA'
 link = 'https://discordapp.com/oauth2/authorize?client_id=389837769293168641&scope=bot&permissions=2146958591'
 
 
