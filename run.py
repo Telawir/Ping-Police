9268c55c379a82709a96450b3bcdae2756e8f611
Telawir <mikeleplacido@interia.pl>
2018-04-26 19:49:05 +0200

    Update run.py

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ import config
 import time
 import os
 
-from config import link, prefix, ownerid, pinglist
+from config import link, prefix, ownerid
 from discord.ext.commands import Bot
 from datetime import datetime, timezone
 
