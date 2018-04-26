84fc64e8435008866b47260ea516da675a090a6d
Telawir <mikeleplacido@interia.pl>
2018-04-26 19:49:19 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,4 @@
 prefix = 'p.'
 ownerid = '215761992474951681'
 link = 'https://discordapp.com/oauth2/authorize?client_id=389837769293168641&scope=bot&permissions=2146958591'
-pinglist = [
-  "<@389837769293168641>",
-  "<@396254726590824450>",
-  "<@355803911447117833>",
-  "<@438819749065916417>"
-]
-
-
 
