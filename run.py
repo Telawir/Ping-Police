73a268ba91a85796c14821677cb416769904f621
Telawir <mikeleplacido@interia.pl>
2018-04-30 10:58:00 +0200

    Update run.py

--- run.py
+++ run.py
@@ -24,24 +24,15 @@ async def on_message(message):
     server = message.server
     await client.change_presence(game=discord.Game(name="Don't ping the devs"))       
     if any(word in message.content for word in["<@224809879884398592>", "<@354641560979111936>", "<@371976663098982400>", "<@311130875461107722>", "<@334269708268470293>", "<@163270868938653698>", "<@281067479927881740>", "<@405654489987547146>", "<@197130820975067137>", "<@249187671912611840>", "<@146009550699364352>", "<@258540501261746176>"]):
-        if message.author.server_permissions.ban_members == False:
+        user_roles = [r.name.lower() for r in ctx.message.author.roles]
+        if not any(r in user_roles for r in["senior moderator", "moderator", "staff"]):
             mem = str(message.author)
             memid = str(message.author.id)
             try:
                 msg = await client.send_message(message.channel, "Don't ping the devs," + " " + str(mem) + " with userid " + str(memid))                    
             except Exception as e:
                 print(e)
-    if any(word in message.content for word in["<@&440432137301131264>"]):
-        member_roles = [r.name.lower() for r in message.author.roles]
-        if message.author.server_permissions.ban_members == True:
-            mem = str(message.author)
-            memid = str(message.author.id)
-            try:
-                msg = await client.send_message(message.channel, "Don't ping the devs," + " " + str(mem) + " with userid " + str(memid))                    
-            except Exception as e:
-                print(e)
-     
-                return 
+
     
 #m2
 @client.command()
