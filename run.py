a6d3f952d31b3672094f2c91d213291703e2d384
Telawir <mikeleplacido@interia.pl>
2018-05-27 19:57:01 +0200

    Update run.py

--- run.py
+++ run.py
@@ -21,30 +21,54 @@ async def on_ready():
     
 @client.event
 async def on_message(message):
+    channel = message.channel
+    channel.id = message.channel.id
     server = message.server
     server.id = message.server.id
+    
     mutedrole = discord.utils.get(server.roles,name="Muted")
     warningrole = discord.utils.get(server.roles,name="Warning")
-    kergo = message.channel
-    kergo = client.get_channel("433644659018039296")
-
-    date = datetime.now().strftime("**Date: **%A, %B %d, %Y\n**Time: **%I:%M:%S %p")
     
+    kergo = message.channel
+    kergo = client.get_channel("433644659018039296") #kogama-log
+    urbo = message.channel
+    urbo = client.get_channel("450347278759100445") #kogama-community-log
+    harpo = message.channel
+    harpo = client.get_channel("444589577341108225")
+    
+    date = datetime.now().strftime("**Date: **%A, %B %d, %Y\n**Time: **%I:%M:%S %p")
     await client.change_presence(game=discord.Game(name="Don't ping the devs"))
+    user_roles = [r.name.lower() for r in message.author.roles]
+    belo = int(server.id)
+    chano = int(channel.id)
+    mem = str(message.author)
+    memid = str(message.author.id)
     
     if message.author.id == client.user.id:
         return
     
+    if message.content == "<@258664801465532416>":
+        if belo == 415885418903371777:
+            if not any(r in user_roles for r in["senior moderator", "moderators", "staff", "kogamate"]):
+                if chano == 436444254173397009:
+                    try:
+                        msg = await client.send_message(message.channel, "Por favor, escreva sua mensagem na mesma linha da menção," + " " + str(mem) + " with userid " + str(memid))                   
+                    except Exception as e:
+                        print(e)
+                else:
+                    pass
+                    #try:
+                    #    msg = await client.send_message(message.channel, "Please ju" + " " + str(mem) + " with userid " + str(memid))                   
+                    #except Exception as e:
+                    #    print(e)
+                    
     if any(word in message.content for word in["<@224809879884398592>", "<@354641560979111936>", "<@371976663098982400>", "<@311130875461107722>", "<@334269708268470293>", "<@163270868938653698>", "<@281067479927881740>", "<@405654489987547146>", "<@197130820975067137>", "<@249187671912611840>", "<@146009550699364352>", "<@258540501261746176>", "<@300978444962103296>"]):
-        user_roles = [r.name.lower() for r in message.author.roles]
-        belo = int(server.id)
-        mem = str(message.author)
-        memid = str(message.author.id)
+
                 
-        if belo == 359426518730145802:
+        if belo == 359426518730145802: #kogama community
             if message.author.server_permissions.ban_members == False:
                 try:
-                    await client.send_message(kergo, str(server) + server.id + '\n' + "**User:** " + mem + " " + memid + '\n' + date)
+                    await client.send_message(urbo, str(server) + server.id + '\n' + "**User:** " + mem + " " + memid + '\n' + date)
                 except:
                     pass
                 try:
@@ -52,19 +76,19 @@ async def on_message(message):
                 except Exception as e:
                     print(e)
                     return
-        else:         
+                
+        if belo == 415885418903371777: #kogama      
             if not any(r in user_roles for r in["senior moderator", "moderators", "staff", "kogamate"]):
                 try:
                     msg = await client.send_message(message.channel, "Don't ping the devs," + " " + str(mem) + " with userid " + str(memid))                   
                 except Exception as e:
                     print(e)
-
                 if "warning" in user_roles:
                     try:
                         await client.add_roles(message.author, mutedrole)
                     except:
                         try:
-                            await client.send_message(kergo, "Server: " + str(server) + ", server id: " + server.id + '\n' + "**User:** " + mem + " " + memid + '\n' + date + '\n' + "**Punishment:** ~~Mute~~ / exeption occured - no punishment")
+                            await client.send_message(kergo, "Server: " + str(server) + ", server id: " + server.id + '\n' + "**Channel**: " + str(channel) + ", channel id: " + channel.id + '\n' + "**User:** " + mem + " " + memid + '\n' + date + '\n' + "**Punishment:** ~~Mute~~ / exeption occured - no punishment" + '\n' + "**Message content**: ```" + str(message.content) + "```")
                         except:
                             pass
                         norolemuted = await client.send_message(message.channel, "``` I can't find Muted role, it's the higher rank than my highest role or I don't have permission to manage roles."  + '\n' + "-- This message will be deleted automatically in 30 seconds. --```")
@@ -75,7 +99,7 @@ async def on_message(message):
                             return
                         return
                     try:
-                        await client.send_message(kergo, "Server: " + str(server) + ", server id: " + server.id + '\n' + "**User:** " + mem + " " + memid + '\n' + date + '\n' + "**Punishment:** Mute")
+                        await client.send_message(kergo, "Server: " + str(server) + ", server id: " + server.id + '\n' + "**Channel**: " + str(channel) + ", channel id: " + channel.id + '\n' + "**User:** " + mem + " " + memid + '\n' + date + '\n' + "**Punishment:** Mute" + '\n' + "**Message content**: ```" + str(message.content) + "```")
                     except:
                         pass
                     warn = await client.send_message(message.channel, message.author.mention + ", you have been muted for disregarding the previous warning and pinging the developer.")
@@ -88,12 +112,12 @@ async def on_message(message):
                         await client.remove_roles(message.author, mutedrole)
                     except:
                         return
-                else:
+                else:   
                     try:
                         await client.add_roles(message.author, warningrole)
                     except Exception as e:
                         try:
-                            await client.send_message(kergo, "Server: " + str(server) + ", server id: " + server.id + '\n' + "**User:** " + mem + " " + memid + '\n' + date + '\n' + "**Punishment:** ~~Warning~~ / exeption occured - no punishment")
+                            await client.send_message(kergo, "Server: " + str(server) + ", server id: " + server.id + '\n' + "**Channel**: " + str(channel) + ", channel id: " + channel.id + '\n' + "**User:** " + mem + " " + memid + '\n' + date + '\n' + "**Punishment:** ~~Warning~~ / exeption occured - no punishment" + '\n' + "**Message content**: ```" + str(message.content) + "```")
                         except:
                             pass
                         norolewarning = await client.send_message(message.channel, "``` I couldn't find Warning role, it's the higher rank than my highest role or I don't have permission to manage roles."  + '\n' + "-- This message will be deleted automatically in 30 seconds. --```")
@@ -103,7 +127,7 @@ async def on_message(message):
                         except:
                             return
                     try:
-                        await client.send_message(kergo, "Server: " + str(server) + ", server id: " + server.id + '\n' + "**User:** " + mem + " " + memid + '\n' + date + '\n' + "**Punishment:** Warning")
+                        await client.send_message(kergo, "Server: " + str(server) + ", server id: " + server.id + '\n' + "**Channel**: " + str(channel) + ", channel id: " + channel.id + '\n' + "**User:** " + mem + " " + memid + '\n' + date + '\n' + "**Punishment:** Warning" + '\n' + "**Message content**: ```" + str(message.content) + "```")
                     except:
                         pass
                         
